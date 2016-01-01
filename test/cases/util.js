832075ca030e2498758353f388603570f0cbef64
ADoyle <adoyle.h@gmail.com>
2016-01-01 16:57:51 +0800

    test: update cases

--- test/cases/util.js
+++ test/cases/util.js
@@ -5,84 +5,44 @@ describe('#util', function() {
 
     describe('returnOrCallback()', function() {
         var err = new Error('this is an error');
-        function func(callback) {
-            setImmediate(callback, null, true);
-        }
-        function func2() {
-            return true;
-        }
-        function func3() {
-            return Promise.resolve(true);
-        }
-        function func4(callback) {
-            setImmediate(callback, err);
-        }
-        function func5() {
-            return err;
-        }
-        function func6() {
-            return Promise.reject(err);
-        }
-        function func7() {
-            throw err;
-        }
 
         it('function invokes callback(null, true)', function() {
-            return util.returnOrCallback(func)
-                .then(function(result) {
-                    result.should.equal(true);
-                });
-        });
-
-        it('function returns true', function() {
-            return util.returnOrCallback(func2)
+            return util.returnOrCallback(function func(callback) {
+                setImmediate(callback, null, true);
+            })
                 .then(function(result) {
                     result.should.equal(true);
                 });
         });
 
         it('function returns a Promise.<true>', function() {
-            return util.returnOrCallback(func3)
+            return util.returnOrCallback(function func3() {
+                return Promise.resolve(true);
+            })
                 .then(function(result) {
                     result.should.equal(true);
                 });
         });
 
         it('function invokes callback(err)', function() {
-            return util.returnOrCallback(func4)
+            return util.returnOrCallback(function func4(callback) {
+                setImmediate(callback, err);
+            })
                 .then(function() {
                     throw new Error('this should not be resolved');
                 }, function(err) {
                     err.should.be.an.Error();
                     err.message.should.equal('this is an error');
-                });
-        });
-
-        it('function returns error', function() {
-            return util.returnOrCallback(func5)
-                .then(function() {
-                    err.should.be.an.Error();
-                    err.message.should.equal('this is an error');
-                }, function() {
-                    throw new Error('this should not be rejected');
                 });
         });
 
         it('function returns a Promise.<|Error>', function() {
-            return util.returnOrCallback(func6)
+            return util.returnOrCallback(function func6() {
+                return Promise.reject(err);
+            })
                 .then(function() {
                     throw new Error('this should not be resolved');
                 }, function(err) {
-                    err.should.be.an.Error();
-                    err.message.should.equal('this is an error');
-                });
-        });
-
-        it('function throw an error', function() {
-            return util.returnOrCallback(func7)
-                .then(function() {
-                    throw new Error('this should not be resolved');
-                }, function() {
                     err.should.be.an.Error();
                     err.message.should.equal('this is an error');
                 });
